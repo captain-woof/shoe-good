58ad1b074e44cf427fb107962a169ba08be1e1c0
Sohail Saha <captainwoof.official@gmail.com>
2021-12-17 08:31:53 +0530

    Category type augmented

--- src/types/query/category.d.ts
+++ src/types/query/category.d.ts
@@ -1,3 +1,11 @@
+import { Category as CategoryChec } from "@chec/commerce.js/types/category"
+
 export type SingleCategoryQueryIdentity = {
     type: "slug" | "id"
+}
+
+declare module "@chec/commerce.js/types/category" {
+    export interface Category extends CategoryChec {
+        assets: {url: string}[]
+    }
 }
